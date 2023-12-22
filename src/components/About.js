685916e2f707f46846caea645ad0c76e52c59b62
Arsen0312 <greenchay16@gmail.com>
2023-12-22 18:35:56 +0600

    HwRouting

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <span style={{margin:'20px'}}>it's my home work</span>
+        </div>
+    )
+}
+
+export default About
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div style={{margin:'20px'}}>
+            2 + 2 = 4
+        </div>
+    )
+}
+
+export default Home
